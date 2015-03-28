3e67173d619b423923774b8c2fb09c955a561e98
pl3bs <brandonchapman@gmx.com>
2015-03-27 19:56:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,7 @@ deb-src http://nginx.org/packages/ubuntu/ trusty nginx
 EOF
 curl http://nginx.org/keys/nginx_signing.key | sudo apt-key add -;
 sudo apt-get update && sudo apt-get -y install nginx;
-cd /tmp;
-cp disco.conf /etc/nginx/conf.d/disco.conf;
+cp /tmp/dontdockmebro/disco.conf /etc/nginx/conf.d/disco.conf;
 sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.disabled;
 mkdir /var/nginx;
 service nginx restart;
